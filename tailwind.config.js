fdf34fb14a3e42a2d3450621a60c0c59f1fa55f6
Val Repsys <valdemaras@gmail.com>
2022-10-13 09:27:29 +0300

    Add css vars for primary, secondary etc colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "color-default": "var(--color-default)",
+      },
+    },
   },
   plugins: [],
 };
