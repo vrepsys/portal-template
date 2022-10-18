8f402b59118ecc3f9bd9500bfa870c99d6029b31
Val Repsys <valdemaras@gmail.com>
2022-10-18 14:11:01 +0300

    Improve page layout

--- components/page-layout/page-layout.tsx
+++ components/page-layout/page-layout.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { PropsWithChildren } from "react";
+import { Fragment, PropsWithChildren } from "react";
 import { Popover } from "@headlessui/react";
 import { SideNavigation } from "../side-navigation/SideNavigation";
 import { TableOfContents } from "../table-of-contents";
@@ -8,6 +8,19 @@ function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
 }
 
+interface RepeatProps extends PropsWithChildren {
+  times: number;
+}
+
+const Repeat: React.FC<RepeatProps> = ({ times, children }) => {
+  return (
+    <>
+      {Array(times)
+        .fill(1)
+        .map(() => children)}
+    </>
+  );
+};
 export const PageLayout: React.FC<PropsWithChildren> = ({ children }) => {
   return (
     <>
@@ -19,54 +32,76 @@ export const PageLayout: React.FC<PropsWithChildren> = ({ children }) => {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="min-h-screen">
-        <Popover
-          as="header"
-          className={({ open }) =>
-            classNames(
-              open ? "inset-0 z-40 overflow-y-auto" : "",
-              "bg-white sticky top-0 lg:border-b lg:border-color-divider lg:overflow-y-visible"
-            )
-          }
-        >
+      <div>
+        <Popover as={Fragment}>
           {({ open }) => (
             <>
-              <div className="mx-auto px-3 py-2 border-b border-color-divider lg:border-none lg:px-10 lg:max-w-[1040px] xl:max-w-[1340px]">
+              <header
+                className={classNames(
+                  open ? "inset-0 z-50 overflow-y-auto" : "",
+                  "bg-white sticky top-0 lg:border-b lg:border-color-divider lg:overflow-y-visible h-14"
+                )}
+              >
+                <div className="mx-auto px-3 py-2 border-b border-color-divider lg:border-none lg:px-10 lg:w-[1040px] xl:w-[1340px] h-14">
+                  <div className="flex gap-2 items-center">
+                    <div className="flex grow gap-2 items-center">
+                      <a className="button" href="#">
+                        Logo
+                      </a>
+                      <a
+                        className="button search w-96 hidden lg:inline-block"
+                        href="#"
+                      >
+                        Search
+                      </a>
+                    </div>
 
-                <div className="flex gap-2 items-center">
-                  <div className="flex grow gap-2 items-center">
-                    <a className="button" href="#">Logo</a>
-                    <a className="button search w-96 hidden lg:inline-block" href="#">Search</a>
-                  </div>
-
-                  <div className="shrink lg:hidden">
-                    <Popover.Button className="button w-9 h-9 flex justify-center items-center">
-                      <span className="sr-only">Open menu</span>
-                      {open ? (
-                        <span className="material-symbols-outlined">close</span>
-                      ) : (
-                        <span className="material-symbols-outlined">menu</span>
-                      )}
-                    </Popover.Button>
-                  </div>
-                  <div className="hidden lg:flex lg:gap-2 lg:items-center lg:shrink lg:justify-end">
-                    <a className="button" href="#">Button one</a>
-                    <a className="button" href="#">Button two</a>
+                    <div className="shrink lg:hidden">
+                      <Popover.Button className="button w-9 h-9 flex justify-center items-center">
+                        <span className="sr-only">Open menu</span>
+                        {open ? (
+                          <span className="material-symbols-outlined">
+                            close
+                          </span>
+                        ) : (
+                          <span className="material-symbols-outlined">
+                            menu
+                          </span>
+                        )}
+                      </Popover.Button>
+                    </div>
+                    <div className="hidden lg:flex lg:gap-2 lg:items-center lg:shrink lg:justify-end">
+                      <a className="button" href="#">
+                        Button one
+                      </a>
+                      <a className="button" href="#">
+                        Button two
+                      </a>
+                    </div>
                   </div>
                 </div>
-              </div>
-
-              <Popover.Panel as="nav" className="px-8 bg-slate-50 lg:hidden" aria-label="Global">
+              </header>
+              <Popover.Panel
+                as="nav"
+                className="fixed h-[calc(100vh_-_theme(spacing.14))] w-screen top-14 overflow-scroll bg-slate-50 px-8 lg:hidden"
+                aria-label="Global"
+              >
                 <div className="flex gap-3 flex-col items-center py-6">
-                  <a className="button search w-full mb-3" href="#">Search</a>
-                  <a className="button w-full text-center" href="#">Button one</a>
-                  <a className="button w-full text-center" href="#">Button two</a>
+                  <a className="button search w-full mb-3" href="#">
+                    Search
+                  </a>
+                  <a className="button w-full text-center" href="#">
+                    Button one
+                  </a>
+                  <a className="button w-full text-center" href="#">
+                    Button two
+                  </a>
                 </div>
-                <hr className="mx-2 bg-color-divider"/>
+                <hr className="mx-2 bg-color-divider" />
                 <div className="pt-3 pb-5">
                   <TableOfContents />
                 </div>
-                <hr className="mx-2 bg-color-divider"/>
+                <hr className="mx-2 bg-color-divider" />
                 <div className="pt-3 pb-5">
                   <SideNavigation />
                 </div>
@@ -74,25 +109,21 @@ export const PageLayout: React.FC<PropsWithChildren> = ({ children }) => {
             </>
           )}
         </Popover>
-        <div>
-          <div className="mx-auto px-5 lg:px-10 lg:grid lg:gap-14 lg:max-w-[1040px] lg:grid-cols-page-tablet xl:max-w-[1340px] xl:grid-cols-page-desktop">
-            <div className="hidden lg:block">
-              <div className="sticky top-12.5 mt-8">
-                <div className="xl:hidden">
-                  <TableOfContents />
-                </div>
-                <SideNavigation />
-              </div>
-            </div>
-            <main className="mt-8 lg:mt-12">
-              {children}
-            </main>
-            <aside className="hidden xl:block">
-              <div className="sticky top-12.5 mt-8 space-y-4">
-                <TableOfContents />
-              </div>
-            </aside>
+
+        <div className="mx-auto px-5 lg:px-10 lg:grid lg:gap-14 lg:w-[1040px] lg:grid-cols-page-tablet xl:w-[1340px] xl:grid-cols-page-desktop">
+          <div className="hidden lg:block border-r border-r-color-divider bg-purple-50 sticky top-14 pt-8 overflow-y-scroll h-[calc(100vh_-_theme(spacing.14))]">
+            <SideNavigation />
           </div>
+          <main className="pb-96 bg-green-100 pt-8">{children}</main>
+          <aside className="hidden lg:block border-r-color-divider bg-fuchsia-50 sticky top-14 pt-8 overflow-y-scroll h-[calc(100vh_-_theme(spacing.14))]">
+            <div className="">
+              <TableOfContents />
+              <Repeat times={40}>
+                Item
+                <br />
+              </Repeat>
+            </div>
+          </aside>
         </div>
       </div>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,26 +8,29 @@ module.exports = {
   ],
   theme: {
     fontFamily: {
-      "sans": ["Inter", "ui-sans-serif", "system-ui"],
-      "serif": ['"Source Serif Pro"',"ui-serif", "Georgia"],
-      "mono": ["ui-monospace", "SFMono-Regular"],
-      "display": ["Oswald"],
-      "body": ["Inter"],
+      sans: ["Inter", "ui-sans-serif", "system-ui"],
+      serif: ['"Source Serif Pro"', "ui-serif", "Georgia"],
+      mono: ["ui-monospace", "SFMono-Regular"],
+      display: ["Oswald"],
+      body: ["Inter"],
     },
     fontSize: {
-      'base': ['1rem', {
-        lineHeight: '1.75rem',
-        letterSpacing: '0.0125rem',
-        fontWeight: '400',
-      }],
+      base: [
+        "1rem",
+        {
+          lineHeight: "1.75rem",
+          letterSpacing: "0.0125rem",
+          fontWeight: "400",
+        },
+      ],
     },
     extend: {
       spacing: {
-        '12.5': '3.25rem',
+        12.5: "3.25rem",
       },
       gridTemplateColumns: {
-        'page-tablet':'15rem 1fr',
-        'page-desktop': '15rem 1fr 15rem',
+        "page-tablet": "15rem 1fr",
+        "page-desktop": "15rem 1fr 15rem",
       },
       colors: {
         "color-default": "var(--color-default)",
@@ -36,62 +39,95 @@ module.exports = {
         "color-divider": "var(--color-divider)",
       },
       fontSize: {
-        'title': ['2.25rem', {
-          lineHeight: '3rem',
-          letterSpacing: '0.025rem',
-          fontWeight: '700',
-        }],
-        'headline-largest': ['1.75rem', {
-          lineHeight: '2.25rem',
-          letterSpacing: '0.025rem',
-          fontWeight: '700',
-        }],
-        'headline-large': ['1.5rem', {
-          lineHeight: '2rem',
-          letterSpacing: '0.025rem',
-          fontWeight: '700',
-        }],
-        'headline-medium': ['1.25rem', {
-          lineHeight: '1.75rem',
-          letterSpacing: '0.025rem',
-          fontWeight: '400',
-        }],
-        'headline-small': ['1rem', {
-          lineHeight: '1.5rem',
-          letterSpacing: '0.05rem',
-          fontWeight: '700',
-        }],
-        'headline-smallest': ['0.875rem', {
-          lineHeight: '1.25rem',
-          letterSpacing: '0.05rem',
-          fontWeight: '700',
-        }],
-        'subtitle': ['1.25rem', {
-          lineHeight: '1.75rem',
-          letterSpacing: '0.0125rem',
-          fontWeight: '400',
-        }],
-        'small': ['0.875rem', {
-          lineHeight: '1.25rem',
-          letterSpacing: '0.0125rem',
-          fontWeight: '400',
-        }],
-        'title-mobile': ['2rem', {
-          lineHeight: '2.5rem',
-          letterSpacing: '0.025rem',
-          fontWeight: '700',
-        }],
-        'headline-largest-mobile': ['1.5rem', {
-          lineHeight: '2rem',
-          letterSpacing: '0.025rem',
-          fontWeight: '700',
-        }],
-        'headline-large-mobile': ['1.25rem', {
-          lineHeight: '1.5rem',
-          letterSpacing: '0.025rem',
-          fontWeight: '700',
-        }],
-      }
+        title: [
+          "2.25rem",
+          {
+            lineHeight: "3rem",
+            letterSpacing: "0.025rem",
+            fontWeight: "700",
+          },
+        ],
+        "headline-largest": [
+          "1.75rem",
+          {
+            lineHeight: "2.25rem",
+            letterSpacing: "0.025rem",
+            fontWeight: "700",
+          },
+        ],
+        "headline-large": [
+          "1.5rem",
+          {
+            lineHeight: "2rem",
+            letterSpacing: "0.025rem",
+            fontWeight: "700",
+          },
+        ],
+        "headline-medium": [
+          "1.25rem",
+          {
+            lineHeight: "1.75rem",
+            letterSpacing: "0.025rem",
+            fontWeight: "400",
+          },
+        ],
+        "headline-small": [
+          "1rem",
+          {
+            lineHeight: "1.5rem",
+            letterSpacing: "0.05rem",
+            fontWeight: "700",
+          },
+        ],
+        "headline-smallest": [
+          "0.875rem",
+          {
+            lineHeight: "1.25rem",
+            letterSpacing: "0.05rem",
+            fontWeight: "700",
+          },
+        ],
+        subtitle: [
+          "1.25rem",
+          {
+            lineHeight: "1.75rem",
+            letterSpacing: "0.0125rem",
+            fontWeight: "400",
+          },
+        ],
+        small: [
+          "0.875rem",
+          {
+            lineHeight: "1.25rem",
+            letterSpacing: "0.0125rem",
+            fontWeight: "400",
+          },
+        ],
+        "title-mobile": [
+          "2rem",
+          {
+            lineHeight: "2.5rem",
+            letterSpacing: "0.025rem",
+            fontWeight: "700",
+          },
+        ],
+        "headline-largest-mobile": [
+          "1.5rem",
+          {
+            lineHeight: "2rem",
+            letterSpacing: "0.025rem",
+            fontWeight: "700",
+          },
+        ],
+        "headline-large-mobile": [
+          "1.25rem",
+          {
+            lineHeight: "1.5rem",
+            letterSpacing: "0.025rem",
+            fontWeight: "700",
+          },
+        ],
+      },
     },
   },
   plugins: [],
