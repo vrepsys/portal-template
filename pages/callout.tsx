a6644dee36bb109024bfb388b71da48b52bcc83d
Domas Markevičius <domas.marko@gmail.com>
2022-10-20 17:24:51 +0300

    Success icon added to callout

--- pages/callout.tsx
+++ pages/callout.tsx
@@ -29,7 +29,7 @@ const CalloutPage: NextPage = () => {
             <div className="text-color-default font-sans text-small text-bold">Please notice that callout should be used with caution and shound not be overused. Too many callouts would make innefective.</div>
         </div>
         <div className="my-3 px-3 py-2 rounded-lg bg-teal-50 border border-teal-200 grid grid-cols-[20px_1fr] gap-3">
-            <svg className="text-teal-500" width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill="currentColor" d="M10 2C5.58172 2 2 5.58172 2 10C2 14.4183 5.58172 18 10 18C14.4183 18 18 14.4183 18 10C18 5.58172 14.4183 2 10 2ZM10 5C10.5523 5 11 5.44772 11 6V10C11 10.5523 10.5523 11 10 11C9.44772 11 9 10.5523 9 10V6C9 5.44772 9.44772 5 10 5ZM10 15C9.44772 15 9 14.5523 9 14C9 13.4477 9.44772 13 10 13C10.5523 13 11 13.4477 11 14C11 14.5523 10.5523 15 10 15Z"/></svg>
+            <svg className="text-teal-500" width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill="currentColor" d="M10 18C14.4183 18 18 14.4183 18 10C18 5.58172 14.4183 2 10 2C5.58172 2 2 5.58172 2 10C2 14.4183 5.58172 18 10 18ZM13.7071 8.70711L9.70711 12.7071C9.31658 13.0976 8.68342 13.0976 8.29289 12.7071L6.29289 10.7071C5.90237 10.3166 5.90237 9.68342 6.29289 9.29289C6.68342 8.90237 7.31658 8.90237 7.70711 9.29289L9 10.5858L12.2929 7.29289C12.6834 6.90237 13.3166 6.90237 13.7071 7.29289C14.0976 7.68342 14.0976 8.31658 13.7071 8.70711Z" /></svg>
             <div className="text-color-default font-sans text-small text-bold">Please notice that callout should be used with caution and shound not be overused. Too many callouts would make innefective.</div>
         </div>
       </article>
