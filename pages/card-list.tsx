9dc28a158407b5dab300a6be11d60aef5cec8f2e
Domas Markevičius <domas.marko@gmail.com>
2022-10-21 15:45:08 +0300

    Updated card hover style

--- pages/card-list.tsx
+++ pages/card-list.tsx
@@ -6,13 +6,15 @@ import TypographyImage from '/public/cards/typography.png';
 
 const Card: React.FC = () => {
   return (
-    <div className="flex flex-col mb-3">
-      <div className="flex overflow-hidden">
-        <Image src={TypographyImage} alt="" aria-hidden={true} />
-      </div>
-      <div className='text-small font-bold mt-2'>Typography</div>
-      <div className='text-small mt-1'>Learn about page title, subtitle, headings, and other typography components</div>
-    </div>
+    <li className='-mx-2'>
+      <a href="#" className="flex flex-col p-2 pb-3 bg-white rounded-xl duration-300 border border-white hover:bg-slate-50 hover:border-slate-100">
+        <div className="flex overflow-hidden">
+          <Image src={TypographyImage} alt="" aria-hidden={true} />
+        </div>
+        <div className='text-small font-bold text-color-default mt-2'>Typography</div>
+        <div className='text-small text-color-secondary mt-1'>Learn about page title, subtitle, headings, and other typography components</div>
+      </a>
+    </li>
   );
 };
 
@@ -25,12 +27,12 @@ const CardsPage: NextPage = () => {
           <PageSubtitle>Learn more about table components in this template</PageSubtitle>
         </header>
         <Heading2>Example</Heading2>
-        <ul className="card-list grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
-          <li><Card /></li>
-          <li><Card /></li>
-          <li><Card /></li>
-          <li><Card /></li>
-          <li><Card /></li>
+        <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 list-none">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
         </ul>
       </article>
     </PageLayout>
