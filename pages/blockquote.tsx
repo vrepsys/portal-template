7bf4d2e4df81e5dafa3cd6dc2ee57c75d9fcf19e
Val Repsys <valdemaras@gmail.com>
2022-10-24 11:04:12 +0300

    Reuse button for search button

--- pages/blockquote.tsx
+++ pages/blockquote.tsx
@@ -1,6 +1,6 @@
-import { NextPage } from "next";
-import { PageLayout } from "../components/page-layout";
-import { PageSubtitle, PageTitle } from "../components/typography";
+import { NextPage } from 'next';
+import { PageLayout } from '../components/page-layout';
+import { PageSubtitle, PageTitle } from '../components/typography';
 
 const BlockQuotePage: NextPage = () => {
   return (
@@ -8,16 +8,22 @@ const BlockQuotePage: NextPage = () => {
       <article>
         <header>
           <PageTitle>Block Quote</PageTitle>
-          <PageSubtitle>
-            Learn more about table components in this template
-          </PageSubtitle>
+          <PageSubtitle>Learn more about table components in this template</PageSubtitle>
         </header>
         <p>Blockquote is here.</p>
         <figure>
-            <blockquote cite="https://www.huxley.net/bnw/four.html" className="border-l-2 pl-4 border-color-tertiary">
-                <p className="text-medium">Words can be like X-rays, if you use them properly—they will go through anything. You read and you are pierced.</p>
-            </blockquote>
-            <figcaption className="text-small text-color-secondary">— Aldous Huxley, <cite>Brave New World</cite></figcaption>
+          <blockquote
+            cite="https://www.huxley.net/bnw/four.html"
+            className="border-l-2 pl-4 border-color-tertiary"
+          >
+            <p className="text-medium">
+              Words can be like X-rays, if you use them properly—they will go through anything. You
+              read and you are pierced.
+            </p>
+          </blockquote>
+          <figcaption className="text-small text-color-secondary">
+            — Aldous Huxley, <cite>Brave New World</cite>
+          </figcaption>
         </figure>
       </article>
     </PageLayout>
