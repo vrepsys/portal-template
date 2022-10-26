dc15729385d59352061693e312a4a9d7bdd6c078
Domas Markevičius <domas.marko@gmail.com>
2022-10-26 19:01:28 +0300

    Minor visual fixes

---
+++ components/icons/IconMenu.tsx
@@ -0,0 +1,10 @@
+export const IconMenu: React.FC = () => {
+    return (
+        <svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+            <path d="M5 4C4.44772 4 4 4.44772 4 5C4 5.55228 4.44772 6 5 6H15C15.5523 6 16 5.55228 16 5C16 4.44772 15.5523 4 15 4H5Z" fill="currentColor"/>
+            <path d="M4 10C4 9.44771 4.44772 9 5 9H15C15.5523 9 16 9.44771 16 10C16 10.5523 15.5523 11 15 11H5C4.44772 11 4 10.5523 4 10Z" fill="currentColor"/>
+            <path d="M5 14C4.44772 14 4 14.4477 4 15C4 15.5523 4.44772 16 5 16H15C15.5523 16 16 15.5523 16 15C16 14.4477 15.5523 14 15 14H5Z" fill="currentColor"/>
+        </svg>        
+    );
+  };
+  
--- components/icons/index.ts
+++ components/icons/index.ts
@@ -2,3 +2,5 @@ export * from './IconNetlify';
 export * from './IconVercel';
 export * from './IconProductHunt';
 export * from './IconGithub';
+export * from './IconDismiss';
+export * from './IconMenu';
