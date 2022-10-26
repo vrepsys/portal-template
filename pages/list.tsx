29499db9177b52f889bd1d1751f2b2de457a1b64
Domas Markevičius <domas.marko@gmail.com>
2022-10-26 14:15:01 +0300

    List component docs

--- pages/list.tsx
+++ pages/list.tsx
@@ -1,7 +1,10 @@
 import { NextPage } from 'next';
+import { CodeBlock } from '../components/code-block';
 import { List } from '../components/list';
 import { PageLayout } from '../components/page-layout';
-import { PageSubtitle, PageTitle } from '../components/typography';
+import { Callout } from '../components/callout';
+import { Link } from '../components/link';
+import { Heading2, PageSubtitle, PageTitle } from '../components/typography';
 
 const ListsPage: NextPage = () => {
   return (
@@ -9,95 +12,41 @@ const ListsPage: NextPage = () => {
       <article>
         <header>
           <PageTitle>Lists</PageTitle>
-          <PageSubtitle>Learn more about table components in this template</PageSubtitle>
+          <PageSubtitle>Learn about List component and how to use it.</PageSubtitle>
         </header>
-        <p>Lists is here.</p>
+        <p>List component shows text in the list. Underneath it's <code>ul</code> and <code>ol</code> html tags underneath. This is how the component looks like:</p>
         <List>
-          <li>
-            <p>Large apples</p>
-          </li>
-          <li>
-            <p>
-              Fresh oranges in a basket full of other tasty fruits that would be amazing for Sunday
-              breakfast. Yummy
-            </p>
-          </li>
-          <li>
-            <p>Tasty perries:</p>
-            <List type="unordered">
-              <li>
-                <p>Great ones</p>
-              </li>
-              <li>
-                <p>Bad ones</p>
-              </li>
-              <li>
-                <p>Dreamers</p>
-              </li>
-              <li>
-                <p>Great discoveries:</p>
-                <List>
-                  <li>
-                    <p>Heaps of treasure</p>
-                  </li>
-                  <li>
-                    <p>Skulls</p>
-                  </li>
-                  <li>
-                    <p>Pirate bones</p>
-                  </li>
-                </List>
-              </li>
-            </List>
-          </li>
-          <li>
-            <p>Sweet raspberries</p>
-          </li>
+          <li><p>Apples</p></li>
+          <li><p>Carrots</p></li>
+          <li><p>Pineaples</p></li>
         </List>
-        <hr />
-        <p>Lists is here.</p>
+        <Heading2>Add list</Heading2>
+        <p>Import the component from <Link target="_blank" href="https://github.com/vrepsys/portal-template/tree/main/components/list">list</Link> folder:</p>
+        <CodeBlock>
+          {"import { List } from '../components/list';"}
+        </CodeBlock>
+        <p>Component ordered and unordered lists. Control it with <code>type</code> prop:</p>
+        <CodeBlock language='javascript'>
+          {`` +
+          `<List type="ordered">\n` +
+          `  <li><p>Buy the groceries</p></li>\n` +
+          `  <li><p>Cook the food</p></li>\n` +
+          `  <li><p>Eat the food</p></li>\n` +
+          `  <li><p>Wash the dishes</p></li>\n` +
+          `<List type="ordered">\n` +
+          ``}
+
+        </CodeBlock>
+        <p>This is how the ordered list looks like:</p>
         <List type="ordered">
-          <li>
-            <p>Large apples</p>
-          </li>
-          <li>
-            <p>
-              Fresh oranges in a basket full of other tasty fruits that would be amazing for Sunday
-              breakfast. Yummy
-            </p>
-          </li>
-          <li>
-            <p>Tasty perries:</p>
-            <List type="ordered">
-              <li>
-                <p>Great ones</p>
-              </li>
-              <li>
-                <p>Bad ones</p>
-              </li>
-              <li>
-                <p>Dreamers</p>
-              </li>
-              <li>
-                <p>Great discoveries:</p>
-                <List type="ordered">
-                  <li>
-                    <p>Heaps of treasure</p>
-                  </li>
-                  <li>
-                    <p>Skulls</p>
-                  </li>
-                  <li>
-                    <p>Pirate bones</p>
-                  </li>
-                </List>
-              </li>
-            </List>
-          </li>
-          <li>
-            <p>Sweet raspberries</p>
-          </li>
+          <li><p>Buy the groceries</p></li>
+          <li><p>Cook the food</p></li>
+          <li><p>Eat the food</p></li>
+          <li><p>Wash the dishes</p></li>
         </List>
+        <Callout type="warning">List component doesn't support <code>ol</code> tag <Link target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/HTML/Element/ol#attributes">attributes</Link>.</Callout>
+        <Heading2>Customize list style</Heading2>
+        <p>Adjust list style in the <Link target="_blank" href="https://github.com/vrepsys/portal-template/blob/main/styles/globals.css">global.css</Link> file.</p>
       </article>
     </PageLayout>
   );
