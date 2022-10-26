58f5b7b0c3c0b74dd30dd8535b01f084e8bb3fe3
Val Repsys <valdemaras@gmail.com>
2022-10-26 14:59:02 +0300

    Add card list content

--- components/code-block/CodeBlock.tsx
+++ components/code-block/CodeBlock.tsx
@@ -1,10 +1,12 @@
 import { CSSProperties, PropsWithChildren } from 'react';
-import { Light as SyntaxHighlighter } from 'react-syntax-highlighter';
-import js from 'react-syntax-highlighter/dist/esm/languages/hljs/javascript';
-import ts from 'react-syntax-highlighter/dist/esm/languages/hljs/typescript';
+import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
+import js from 'react-syntax-highlighter/dist/esm/languages/prism/javascript';
+import ts from 'react-syntax-highlighter/dist/esm/languages/prism/typescript';
+import tsx from 'react-syntax-highlighter/dist/esm/languages/prism/tsx';
 
 SyntaxHighlighter.registerLanguage('javascript', js);
 SyntaxHighlighter.registerLanguage('typescript', ts);
+SyntaxHighlighter.registerLanguage('typescript', tsx);
 
 interface Props extends PropsWithChildren {
   language: string;
--- pages/card-list.tsx
+++ pages/card-list.tsx
@@ -1,6 +1,7 @@
 import { NextPage } from 'next';
 import { Card, CardList } from '../components/cards';
 import { CardsExample } from '../components/cards-example';
+import { CodeBlock } from '../components/code-block';
 import { PageLayout } from '../components/page-layout';
 import { Heading2, PageSubtitle, PageTitle } from '../components/typography';
 
@@ -12,6 +13,17 @@ const CardsPage: NextPage = () => {
           <PageTitle>Card list</PageTitle>
           <PageSubtitle>Learn about the CardList and Card components</PageSubtitle>
         </header>
+        <p>
+          To render cards use the <code>CardList</code>, and <code>Card</code> components.
+        </p>
+        <p>Here&apos;s a code example:</p>
+        <CodeBlock language="typescript">
+          {'<CardList>\n' +
+            '   <Card title="Typography" subtitle="Get familiar with typography components" image="typography.png" url="/typography" />\n' +
+            '   <Card title="List" subtitle="Learn how lists work" image="list.png" url="/list" />\n' +
+            '<CardList>'}
+        </CodeBlock>
+
         <Heading2>Example</Heading2>
         <CardsExample />
       </article>
