f69eaf9b8d1d55a99de72b39a0c7ee06f99063dd
Val Repsys <valdemaras@gmail.com>
2022-10-26 14:44:35 +0300

    Improve callout page

--- pages/callout.tsx
+++ pages/callout.tsx
@@ -11,32 +11,51 @@ const CalloutPage: NextPage = () => {
       <article>
         <header>
           <PageTitle>Callout</PageTitle>
-          <PageSubtitle>Learn about Callout component and how to use it.</PageSubtitle>
+          <PageSubtitle>Learn about Callout component and how to use it</PageSubtitle>
         </header>
-        <p>Callout component has a distinct look meant to drag reader's attention, so critical information could be noticed. This is how the component looks like:</p>
-        <Callout type="info">
-          This message is just for a demo purpose to show the component's look. 
-        </Callout>
+        <p>
+          Callout component has a distinct look meant to draw reader&apos;s attention, so that
+          critical information is more noticeable. This is how the component looks like:
+        </p>
+        <Callout type="info">This is how a callout looks like</Callout>
         <Heading2>Add callout</Heading2>
-        <p>Import the component from <Link target="_blank" href="#">callout</Link> folder:</p>
-        <CodeBlock>
+        <p>
+          Import the component from the{' '}
+          <Link target="_blank" href="#">
+            callout
+          </Link>{' '}
+          folder:
+        </p>
+        <CodeBlock language="javascript">
           {"import { Callout } from '../components/callout';"}
         </CodeBlock>
-        <p>Specify callout style using <code>type</code> prop. It supports 4 values: <code>info</code>,<code>caution</code>,<code>warning</code>,<code>success</code>:</p>
-        <CodeBlock language='javascript'>
-          {`` +
-          `<Callout type="info">\n` +
-          `  Callout message\n` +
-          `</Callout>` +
-          ``}
+        <p>
+          Specify the callout style using the <code>type</code> prop. It supports 4 values:{' '}
+          <code>info</code>,<code>caution</code>,<code>warning</code>,<code>success</code>:
+        </p>
+        <CodeBlock language="javascript">
+          {`` + `<Callout type="info">\n` + `  Callout message\n` + `</Callout>` + ``}
         </CodeBlock>
-        <p>Here is the visual style of each type:</p>
+        <p>Examples:</p>
         <Callout type="info">Info — hint or general information.</Callout>
-        <Callout type="caution">Caution — critical information telling what's blocking.</Callout>
-        <Callout type="warning">Warning — important information that may affect user's workflow.</Callout>
-        <Callout type="success">Success – message tells that all works.</Callout>
-        <Heading2>Customize callout style</Heading2>
-        <p>Adjust component's style in the <Link target="_blank" href="https://github.com/vrepsys/portal-template/blob/main/components/callout/Callout.tsx">Callout.tsx</Link> file.</p>
+        <Callout type="caution">
+          Caution — critical information telling what&apos;s blocking.
+        </Callout>
+        <Callout type="warning">
+          Warning — important information that may affect user&apos;s workflow.
+        </Callout>
+        <Callout type="success">Success — message tells that all works.</Callout>
+        <Heading2>Customize callout appearance</Heading2>
+        <p>
+          Adjust component&apos;s style by modifying the{' '}
+          <Link
+            target="_blank"
+            href="https://github.com/vrepsys/portal-template/blob/main/components/callout/Callout.tsx"
+          >
+            Callout.tsx
+          </Link>
+          .
+        </p>
       </article>
     </PageLayout>
   );
